febae888a71cf69f68061c684104f1d8cbfe31dc
teotoivo <teo.maximilien@gmail.com>
2025-03-31 19:54:25 +0300

    fixed

--- src/main.c
+++ src/main.c
@@ -1,21 +1,16 @@
-#include "/usr/arm-none-eabi/include/stdio.h"
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 int main() {
-  const uint led_pin = 22;
+  const uint led_pin = 15; // <-- onboard LED for test
   uint count = 0;
 
-  // Initialize LED pin
   gpio_init(led_pin);
   gpio_set_dir(led_pin, GPIO_OUT);
 
-  // Initialize chosen serial port
   stdio_init_all();
 
-  // Loop forever
   while (true) {
-
-    // Blink LED
     gpio_put(led_pin, true);
     printf("Blinking! %u\r\n", ++count);
     sleep_ms(1000);
